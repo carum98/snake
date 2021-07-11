aa1a069713d1815ec24f70259eddcc6e20b4d5ec
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-07-10 21:14:26 -0600

    Snake move

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/gestures.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,30 +12,75 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(child: SnakeGame()),
+      home: SnakePage(),
     );
   }
 }
 
-class SnakeGame extends StatelessWidget {
-  const SnakeGame({Key? key}) : super(key: key);
+class SnakePage extends StatelessWidget {
+  const SnakePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Container(
+        color: Colors.black,
+        child: SnakeGame(
+          size: MediaQuery.of(context).size,
+        ),
+      ),
+    );
+  }
+}
 
-    final axisX = size.width ~/ 20;
-    final axixY = ((size.height - kToolbarHeight) ~/ 20) * axisX;
+class SnakeGame extends StatefulWidget {
+  final Size size;
+  const SnakeGame({Key? key, required this.size}) : super(key: key);
 
+  @override
+  _SnakeGameState createState() => _SnakeGameState();
+}
+
+class _SnakeGameState extends State<SnakeGame> {
+  var snake = [35, 55, 75, 95];
+
+  late int axisX;
+  late int axisY;
+
+  @override
+  void initState() {
+    super.initState();
+
+    axisX = widget.size.width ~/ 20;
+    axisY = ((widget.size.height - kToolbarHeight) ~/ 20) * axisX;
+
+    Timer.periodic(Duration(milliseconds: 500), (_) {
+      updateSnake();
+    });
+  }
+
+  void updateSnake() {
+    if (snake.last > axisY) {
+      snake.add(snake.last - axisY);
+    } else {
+      snake.add(snake.last + axisX);
+    }
+
+    snake.removeAt(0);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return GridView.builder(
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: axixY,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: axisY,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: axisX,
       ),
       itemBuilder: (context, index) => Container(
         margin: EdgeInsets.all(2),
-        color: Colors.grey,
+        color: snake.contains(index) ? Colors.green : Colors.grey[200],
       ),
     );
   }
